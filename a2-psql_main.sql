1e461784296e0b2541720ab45e71ad00293048e1
Mursalin <mursalin2311@gmail.com>
2025-05-28 15:27:36 +0600

    4 problem solved

--- a2-psql_main.sql
+++ a2-psql_main.sql
@@ -1,8 +1,23 @@
 -- 1.Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
 
+INSERT INTO rangers(name,region) VALUES('Derek Fox','Coastal Plains');
+
+
+
 --2.Count unique species ever sighted.
+
+SELECT COUNT(DISTINCT species_id) AS unique_species_count FROM sightings;
+
 --3.Find all sightings where the location includes "Pass".
+SELECT * FROM sightings
+ WHERE location LIKE '%Pass%';
+
 --4.List each ranger's name and their total number of sightings
+SELECT name, COUNT(*) AS total_sightings
+FROM rangers 
+JOIN sightings  ON rangers.ranger_id = sightings.ranger_id
+GROUP BY name; 
+
 --5.List species that have never been sighted.
 --6.Show the most recent 2 sightings.
 --7.Update all species discovered before year 1800 to have status 'Historic'.
