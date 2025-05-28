b2abe3b1f8da3be515d024043f1ba1e8399f75a9
Mursalin <mursalin2311@gmail.com>
2025-05-28 14:41:25 +0600

    created main file and added all 9 problems

---
+++ a2-psql_main.sql
@@ -0,0 +1,10 @@
+-- 1.Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+
+--2.Count unique species ever sighted.
+--3.Find all sightings where the location includes "Pass".
+--4.List each ranger's name and their total number of sightings
+--5.List species that have never been sighted.
+--6.Show the most recent 2 sightings.
+--7.Update all species discovered before year 1800 to have status 'Historic'.
+--8.Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+--9.Delete rangers who have never sighted any species
