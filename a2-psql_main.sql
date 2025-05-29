a35747ef973884d20e7024bad8cc66bdf9f89e86
Mursalin <mursalin2311@gmail.com>
2025-05-29 11:21:39 +0600

    ok all 9 problem solved

--- a2-psql_main.sql
+++ a2-psql_main.sql
@@ -29,6 +29,22 @@ JOIN species ON species.species_id = sightings.species_id
 JOIN rangers ON rangers.ranger_id = sightings.ranger_id
 ORDER BY sighting_time DESC
 LIMIT 2;
+
 --7.Update all species discovered before year 1800 to have status 'Historic'.
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01'
+
 --8.Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+SELECT sighting_id,
+CASE 
+    WHEN EXTRACT(HOUR FROM sighting_time) <12  THEN 'Morning'
+    WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 and 17 THEN 'Afternoon'
+    ELSE  'Evening'
+END
+as time_of_day
+from sightings;
+
 --9.Delete rangers who have never sighted any species
+DELETE FROM rangers
+WHERE ranger_id NOT IN (SELECT DISTINCT ranger_id FROM sightings);
